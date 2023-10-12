ad196a134d09fb942b54446a5ee77999ebb80c58
dragonfly9999 <henwir42@gmail.com>
2023-10-12 15:19:09 +0700

    chi Thy check nha

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// thanh nav co giãn
+
 window.onscroll = function () {
-    myFunction();
-  };
-  
-  var header = document.getElementById("navFix");
-  var sticky = header.offsetTop;
-  
-  function myFunction() {
-    if (window.pageYOffset === 0) {
-      header.classList.remove("sticky-theme");
-    } else {
-      header.classList.add("sticky-theme");
-    }
+  myFunction();
+};
+
+var header = document.getElementById("navFix");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset === 0) {
+    header.classList.remove("sticky-theme");
+  } else {
+    header.classList.add("sticky-theme");
   }
+}
+
+// chế độ sáng tối
+var nutToi = document.getElementById("cheDoToi");
+var nutSang = document.getElementById("cheDoSang");
+nutSang.style.display = "none";
+
+document.getElementById('cheDoToi').onclick = function(){
+  nutSang.style.display = 'inline-block'
+  nutToi.style.display = 'none'
+}
+document.getElementById('cheDoSang').onclick = function(){
+  nutSang.style.display = 'none'
+  nutToi.style.display = 'inline-block'
+}
